d0aed686fc727ae5db1d6e9647aa1d6e1496beea
sdigit87 <soubhikadi@gmail.com>
2017-01-02 14:11:10 +0100

    div hard float supported

---
+++ benchmark/div/src/code_hard_float.c
@@ -0,0 +1,13 @@
+unsigned int div_hard_float[] = {
+	0xa8000022,
+	0xa8000043,
+	0xa8000004,
+	0xa0000005,
+	0xa1000006,
+	0x100014c1,
+	0x74001024,
+	0xc2000c83,
+	0x7c000823,
+	0x92000000
+};
+unsigned *code_hard_float = div_hard_float;
---
+++ benchmark/div/src/code_hard_float.h
@@ -0,0 +1,2 @@
+#define DIV_HARD_FLOAT_LEN                  10
+#define DIV_HARD_FLOAT_POS                  0
